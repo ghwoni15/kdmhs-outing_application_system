d19454990c0f47a98d16e5ab70b7a52626dd1aeb
ghwoni15 <dimiwoni12dev@gmail.com>
2015-12-04 17:40:19 +0900

    deleting stuff

--- include/auth.php
+++
@@ -1,4 +0,0 @@
-<?php
-        $link = mysqli_connect("localhost", "outing", "outing00", "outing_new") or die("<script>location.href='./error.php?errno=0&errmsg=Connection_Failed';</script>\n");
-        mysqli_set_charset($link, "utf8");
-?>
